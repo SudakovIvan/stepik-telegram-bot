e71d33e42661929888a9103ed298d4d85145ae77
Судаков Иван Анатольевич <ias@nicetu.spb.ru>
2020-09-04 17:56:32 +0300

    use pyTelegramBotAPI

--- main.py
+++ main.py
@@ -1,54 +1,36 @@
+import telebot
 import requests
-import time
 
 token = "1338383686:AAHTzCQAg345W3nCl6GTy7n8mWN4IwLdxUE"
-
 # https://api.telegram.org/bot1338383686:AAHTzCQAg345W3nCl6GTy7n8mWN4IwLdxUE/sendmessage?chat_id=943519774&text=hi
-URL = "https://api.telegram.org/bot" + token + "/"
+
+bot = telebot.TeleBot(token)
+
+question_counter = 0
 
 
-def get_updates():
-    url = URL + "getupdates"
-    r = requests.get(url)
-    return r.json()
-
-
-def get_message():
-    data = get_updates()
-    chat_id = data["result"][-1]["message"]["chat"]["id"]
-    message_text = data["result"][-1]["message"]["text"]
-    update_id = data["result"][-1]["update_id"]
-    message = {"chat_id": chat_id, "text": message_text, "update_id": update_id}
-    return message
-
-
-def send_message(chat_id, text="Wait a second please..."):
-    url = URL + "sendmessage?chat_id={}&text={}".format(chat_id, text)
-    requests.get(url)
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    if message.text == "/start":
+        bot.reply_to(message, "Напиши 'Дай вопрос' или 'Сколько вопросов'")
+    elif message.text == "Привет":
+        bot.reply_to(message, "Привет, " + message.from_user.first_name)
+    elif message.text == "Дай вопрос":
+        global question_counter
+        question_counter += 1
+        original_reply = str(requests.get("https://engine.lifeis.porn/api/millionaire.php?q=2").json())
+        # что за непонятные символы? Это потому, что бесплатно?
+        corrected_reply = original_reply.replace(u"\\u2063","")
+        bot.reply_to(message, corrected_reply)
+    elif message.text == "Сколько вопросов":
+        bot.reply_to(message, "Вопросов выдано: {}".format(question_counter))
+    else:
+        bot.reply_to(message, "Я не знаю таких слов: '" + message.text + "'")
 
 
 def main():
-    # with open("updates.json", "w") as file:
-    #     json.dump(updates, file, indent=2, ensure_ascii=False)
-    prev_update_id = None
-    while True:
-        answer = get_message()
-        current_update_id = answer["update_id"]
-        print(current_update_id, prev_update_id)
-
-        if prev_update_id != current_update_id:
-            if answer["text"] == "pic":
-                text_to_send = requests.get("https://loremflickr.com/320/240", allow_redirects=True).url
-            else:
-                text_to_send = "Type 'pic' to get a new picture"
-            send_message(answer["chat_id"], text_to_send)
-            prev_update_id = current_update_id
-
-        time.sleep(1)
+    bot.polling()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
