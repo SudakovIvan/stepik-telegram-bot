d4c5ecfbd8fe1222f0e47c8a808362c0c9445ecb
Судаков Иван Анатольевич <ias@nicetu.spb.ru>
2020-09-10 07:58:20 +0300

    Настройки для каждого пользователя свои

--- main.py
+++ main.py
@@ -8,23 +8,27 @@ token = "1338383686:AAHTzCQAg345W3nCl6GTy7n8mWN4IwLdxUE"
 bot = telebot.TeleBot(token)
 trivia_api = pytrivia.Trivia(with_token=True)
 
-states = {}
 MAIN_STATE = "main_state"
 GAME_STATE = "game_state"
 SET_QUESTION_COUNT_STATE = "question_count_state"
 SET_DIFFICULTY_STATE = "difficulty_state"
 
+DEFAULT_SETTINGS = {"question_count": 3,
+                    "difficulty": pytrivia.Diffculty.Easy,
+                    "category": pytrivia.Category.Computers
+                    }
+
+states = {}
 questions = {}
-settings = {"question_count": 3,
-            "difficulty": pytrivia.Diffculty.Easy,
-            "category" : pytrivia.Category.Computers
-            }
+settings = {}
 
 
 def initialize_game(user_id):
-    api_reply = trivia_api.request(settings["question_count"],
-                                   settings["category"],
-                                   settings["difficulty"],
+    current_settings = get_current_settings(user_id)
+
+    api_reply = trivia_api.request(current_settings["question_count"],
+                                   current_settings["category"],
+                                   current_settings["difficulty"],
                                    pytrivia.Type.Multiple_Choice)
 
     questions[user_id] = collections.deque()
@@ -39,6 +43,13 @@ def send_next_question(user_id):
     bot.send_message(user_id, text)
 
 
+def get_current_settings(user_id):
+    if user_id not in settings:
+        # тут же  я правильно скопировал это дело? Иначе будет один объект для всех пользователей?
+        settings[user_id] = dict(DEFAULT_SETTINGS)
+    return settings[user_id]
+
+
 @bot.message_handler(func=lambda message: states.get(message.from_user.id, MAIN_STATE) == MAIN_STATE)
 def main_handler(message):
     text = message.text.lower()
@@ -50,10 +61,12 @@ def main_handler(message):
         if not questions[user_id]:
             bot.reply_to(message, "Не удалось получить ни одного вопроса")
         else:
+            current_settings = get_current_settings(user_id)
             start_game_message = "Это беспроигрышная пока игра. Можно отвечать что угодно:)\n" \
                                  "Количество вопросов: {0}\n" \
                                  "Сложность: {1}\n" \
-                                 "Категория: {2}".format(settings["question_count"], settings["difficulty"], settings["category"])
+                                 "Категория: {2}".format(current_settings["question_count"],
+                                                         current_settings["difficulty"], current_settings["category"])
 
             bot.reply_to(message, start_game_message)
             send_next_question(user_id)
@@ -78,10 +91,12 @@ def game_handler(message):
 
 @bot.message_handler(func=lambda message: states.get(message.from_user.id, MAIN_STATE) == SET_QUESTION_COUNT_STATE)
 def set_question_count_handler(message):
-    settings["question_count"] = int(message.text)
+    user_id = message.from_user.id
+    current_settings = get_current_settings(user_id)
+    current_settings["question_count"] = int(message.text)
     bot.reply_to(message, "Принял")
-    bot.send_message(message.from_user.id, "Выберем сложность вопросов ('легко', 'средне', 'сложно')")
-    states[message.from_user.id] = SET_DIFFICULTY_STATE
+    bot.send_message(user_id, "Выберем сложность вопросов ('легко', 'средне', 'сложно')")
+    states[user_id] = SET_DIFFICULTY_STATE
 
 
 @bot.message_handler(func=lambda message: states.get(message.from_user.id, MAIN_STATE) == SET_DIFFICULTY_STATE)
@@ -89,13 +104,15 @@ def set_difficulty_handler(message):
     text = message.text.lower()
     reply_text = "Принял"
     new_state = MAIN_STATE
+    user_id = message.from_user.id
+    current_settings = get_current_settings(user_id)
 
     if text == "легко":
-        settings["difficulty"] = pytrivia.Diffculty.Easy
+        current_settings["difficulty"] = pytrivia.Diffculty.Easy
     elif text == "средне":
-        settings["difficulty"] = pytrivia.Diffculty.Medium
+        current_settings["difficulty"] = pytrivia.Diffculty.Medium
     elif text == "сложно":
-        settings["difficulty"] = pytrivia.Diffculty.Hard
+        current_settings["difficulty"] = pytrivia.Diffculty.Hard
     else:
         reply_text = "Надо выбрать из трех вариантов: 'легко', 'средне', 'сложно'"
         new_state = None
@@ -103,7 +120,7 @@ def set_difficulty_handler(message):
     bot.reply_to(message, reply_text)
 
     if new_state is not None:
-        states[message.from_user.id] = new_state
+        states[user_id] = new_state
 
 
 def main():
